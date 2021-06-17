cc79d9f12513bcc8e3d4e0d043c8568c77bca424
Sudheer Tripathi <tripathisudheer604@gmail.com>
2021-06-17 22:06:21 +0530

    dist gitignored

--- dist/helpers/DataNester.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export default class DataNester {
-//     public static dataNester() : ExplorerItem {
-//     }
-// }
--- dist/models/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-module.exports = {
-    Client: require("./Client"),
-    Project: require("./Project"),
-    CostType: require("./CostType"),
-    Cost: require("./Cost")
-};
